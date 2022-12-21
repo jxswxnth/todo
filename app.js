105d0e72d540ca5b5e520f977b9ce30996fe91d3
jxswxnth <jaswanthsai40@gmail.com>
2022-12-21 19:16:48 +0530

    Add MongoDB and custom lists

--- app.js
+++ app.js
@@ -1,49 +1,146 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname+"/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 const port = process.env.PORT;
-let tasks = ["Wake Up", "Drink water","Walk a bit"];
-let works = [];
+const now = date.getDate();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
+mongoose.set({
+  strictQuery: true
+});
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", itemSchema);
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema] //array of itemSchema based items.
+})
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item({
+  name: "Wake up"
+})
+const item2 = new Item({
+  name: "Drink water"
+})
+const item3 = new Item({
+  name: "Walk a bit"
+})
+const defaultItems = [item1, item2, item3];
+
 app.get('/', (req, res) => {
-  today = date.getDate();
-  res.render('pages/list', {
-    heading: today,
-    listOfTasks: tasks
-  });
-})
-app.post('/', (req,res)=>{
-  item = req.body.newTask;
-    if(req.body.taskORwork==="WorkList"){ //taskORwork is name of submit button and
-      // we can get value from that as per 'heading'
-      works.push(item);
-      res.redirect('/work');
-    } else {
-      tasks.push(item);
-      res.redirect('/');
+  Item.find((err, results) => {
+    if (!err) {
+      if (results.length === 0) {
+        Item.insertMany(defaultItems, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("3 items inserted.");
+          }
+        })
+        res.redirect('/');
+      } else {
+        res.render('pages/list', {
+          heading: "Tasks",
+          listOfItems: results,
+          today: now
+        });
+      }
     }
+  });
+});
+
+app.post('/', (req,res)=>{
+  let item = req.body.newItem;
+  let todo = req.body.toDoList;
+
+  const newItem = new Item({
+    name: item
+  });
+  if (todo === "Tasks") {
+    newItem.save();
+    res.redirect('/');
+  } else {
+    List.findOne({
+      name: todo
+    }, (err, foundList) => {
+      if (!err) {
+        if (foundList) {
+          foundList.items.push(newItem);
+          foundList.save();
+          res.redirect('/' + todo);
+        }
+      }
+    });
+  }
 })
 
+app.post('/delete', (req, res) => {
+  let deleteID = req.body.idToDelete;
+  let ListName = req.body.ListName;
 
-app.get("/work",(req,res)=>{
-  res.render('pages/list',{
-    heading: "WorkList",
-    listOfTasks: works
-  })
-})
+  if (ListName === "Tasks") {
+    Item.deleteOne({
+      _id: deleteID
+    }, (err) => {
+      if (!err) {
+        res.redirect('/');
+      }
+    });
+  } else {
+    List.findOneAndUpdate({
+      name: ListName
+    }, {
+      $pull: {
+        items: {
+          _id: deleteID
+        }
+      }
+    }, (err, result) => {
+      if (!err) {
+        res.redirect('/' + ListName);
+      }
+    });
+  }
+});
 
-app.get('/about', (req,res)=>{
-  res.render('pages/about',{
-    heading: "About"
-  })
-})
+app.get('/:listFromUser', (req, res) => {
+  const customList = _.capitalize(req.params.listFromUser);
+  const newList = new List({
+    name: customList,
+    items: []
+  });
+  List.findOne({
+    name: customList
+  }, (err, result) => {
+    if (!err) {
+      if (result) {
+        res.render('pages/list', {
+          heading: result.name,
+          listOfItems: result.items,
+          today: now
+        });
+      } else {
+        newList.save(); //saving new route in db
+        res.redirect('/' + customList); // redirecting to saved route
+      }
+    }
+  });
+});
 
 app.listen(port, (req, res) => {
   console.log(`App is listening at port ${port}`)
